f56cfce4434753185f401772c5e59744c66a6596
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-08 17:30:03 +0530

    Dropdown Menus Completed

--- src/app/Components/search-input/search-input.component.ts
+++ src/app/Components/search-input/search-input.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataServiceComponent} from "./data-service/data-service.component";
+import {Observable} from "rxjs";
+
 
 @Component({
   selector: 'app-search-input',
@@ -8,26 +10,12 @@ import {DataServiceComponent} from "./data-service/data-service.component";
   providers: [DataServiceComponent]
 })
 export class SearchInputComponent implements OnInit {
+  public keyword = "name";
+  public data$: Observable<any[]> = new Observable<any[]>();
 
   constructor(private dataSvc: DataServiceComponent) {
     this.getData();
   }
-
-  public keyword = "";
-  data = [
-    {
-      id: 1,
-      name: "A"
-    },
-    {
-      id: 2,
-      name: "B"
-    },
-    {
-      id: 3,
-      name: "C"
-    },
-  ]
 
   selectEvent(item: any) {
     // do something with selected item
@@ -45,8 +33,8 @@ export class SearchInputComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getData(){
-    this.dataSvc.getAllCountries().subscribe(console.log);
+  getData(): void{
+      this.data$ = this.dataSvc.getAllCountries();
   }
 
 
