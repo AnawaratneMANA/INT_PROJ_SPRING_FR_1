87a553e03afff5ae4a800163b2877a64a458272c
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-08 13:14:38 +0530

    UI created with Custom CSS Input Fields Completed.

--- src/app/Components/validation-status/validation-status.component.ts
+++ src/app/Components/validation-status/validation-status.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-validation-status',
   templateUrl: './validation-status.component.html',
-  styleUrls: ['./validation-status.component.css']
+  styleUrls: ['./validation-status.component.scss']
 })
 export class ValidationStatusComponent implements OnInit {
 
