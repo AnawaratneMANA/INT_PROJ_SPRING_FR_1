eab5957273fcd4c54e18ae74d9c09b21f0bf0267
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-27 11:45:25 +0530

    Delete Functional Work Properly delete items from the table.

--- src/app/Components/search-input/project-user-table/project-user-table.component.ts
+++ src/app/Components/search-input/project-user-table/project-user-table.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {Form, FormGroup} from "@angular/forms";
+import { HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-project-user-table',
@@ -9,7 +10,7 @@ import {Form, FormGroup} from "@angular/forms";
 export class ProjectUserTableComponent implements OnInit, OnChanges{
   @Input() userDetailsForm: FormGroup;
   @Input() row: Array<any> = [];
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -17,6 +18,27 @@ export class ProjectUserTableComponent implements OnInit, OnChanges{
   ngOnChanges(row) {
     console.log("This is coming from the table class.")
     console.log(row);
+  }
+
+  //When Delete Button is Pressed.
+  deleteItem(object){
+    console.log("Item Delete!" + object);
+    this.row.forEach((value, index) => {
+      if(value == object){
+        this.row.splice(index, 1)
+      }
+    })
+    console.log(this.row);
+  }
+
+  SaveDataInDataBase(object){
+    const url = 'http://localhost:8080/api/v1/form';
+    this.httpClient.post(url, object)
+      .subscribe(
+        (result) => {
+          this.ngOnInit();
+        }
+      )
   }
 
 
