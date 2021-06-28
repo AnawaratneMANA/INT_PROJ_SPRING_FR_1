092b80256ffea3937085e434c8e0f0b52eb87011
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-06-28 08:46:04 +0530

    CrossOrigin Error exist.

--- src/app/Components/search-input/project-user-table/project-user-table.component.ts
+++ src/app/Components/search-input/project-user-table/project-user-table.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {Form, FormGroup} from "@angular/forms";
-import { HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-project-user-table',
@@ -10,6 +10,12 @@ import { HttpClient} from "@angular/common/http";
 export class ProjectUserTableComponent implements OnInit, OnChanges{
   @Input() userDetailsForm: FormGroup;
   @Input() row: Array<any> = [];
+
+  //Setting Headers to HttpClient.
+  header = new HttpHeaders({
+    "token": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYW1lIiwiZXhwIjoxNjI0ODE4OTAzLCJpYXQiOjE2MjQ3ODI5MDN9.0nSmWKNG8mk_LsiYwps7JnjEWHgAKZms0-MZYY5S96E",
+    'Access-Control-Allow-Origin': '*'
+  })
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
@@ -32,11 +38,12 @@ export class ProjectUserTableComponent implements OnInit, OnChanges{
   }
 
   SaveDataInDataBase(object){
+    console.log(object);
     const url = 'http://localhost:8080/api/v1/form';
     this.httpClient.post(url, object)
       .subscribe(
         (result) => {
-          this.ngOnInit();
+         alert("Data Inserted Successfully" + result);
         }
       )
   }
